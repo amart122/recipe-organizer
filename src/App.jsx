06da0888506ee736d93a381066a1583c8d21bb17
Aram Martinez <amart122@asu.edu>
2024-05-19 17:44:19 -0700

    Introduced theme toggler

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css'
 import '@picocss/pico/css/pico.min.css';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
 function App() {
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  useEffect(() => {
+    document.getElementsByTagName('html')[0].dataset.theme = theme;
+    document.getElementsByTagName('main')[0].dataset.theme = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme])
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
 
   return (
     <main className='main-container container'>
-      <Navbar/>
+      <Navbar toggleTheme={toggleTheme}/>
 
       <Routes>
         <Route path='/' element={<Home />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,18 @@ import "../assets/css/Navbar.css";
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function NavBar() {
+function NavBar({ toggleTheme }) {
 
   return (
     <nav>
       <ul>
         <li>
           <Link to='/'>Home</Link>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <button onClick={toggleTheme}>Toggle Theme</button>
         </li>
       </ul>
     </nav>
