e6e929971d6d6ab3f2f6b107b7ff5338e0f3129d
Aram Martinez <amart122@asu.edu>
2024-05-19 17:37:10 -0700

    Removed creation styles, icons and HTML Introduced initial layout and components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import "../assets/css/Navbar.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function NavBar() {
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import "../assets/css/Home.css";
+
+function Home() {
+  return (
+    <div className='main-home'>
+    </div>
+  );
+}
+
+export default Home; 
